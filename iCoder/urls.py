859d442de69b2f66f927afa86f833ddde75e8307
Khushal <khushalraiyani2@gmail.com>
2023-03-01 20:55:33 +0530

    Initial Commit2

--- iCoder/urls.py
+++ iCoder/urls.py
@@ -15,6 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 admin.site.site_header="iCoder Admin"
 admin.site.site_title="iCoder Admin Panel"
@@ -25,3 +26,5 @@ urlpatterns = [
     path('', include('home.urls')),
     path('blog/', include('blog.urls')),
 ]
+
+urlpatterns += staticfiles_urlpatterns()
